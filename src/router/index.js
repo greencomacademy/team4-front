c71b3b161952057aed5a37bbc55e893a043609bd
coding_babo <kwondevelop@gmail.com>
2026-06-15 18:01:48 +0900

    main.css 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,66 +1,38 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import DashboardView from '../views/DashboardView.vue'
-import Registration from '../views/auth/Registration.vue'
-import { useAuthStore } from '../store/useAuthStore.js'
-import App from '../App.vue'
-import PlatformsView from '../views/PlatformsView.vue'
-
-const setMeta = (isAuthenticated, isGestOnly) => {
-  return {
-    isAuthenticated,
-    isGestOnly,
-  }
-}
+import { createRouter, createWebHistory } from 'vue-router';
+import LandingView from '../views/LandingView.vue';
+import PlatformsView from '../views/PlatformsView.vue';
+import { useAuthStore } from '../store/useAuthStore';
 
 const routes = [
   {
-    path: '/auth',
-    component: App
-  },
-  {
     path: '/',
-    component: DashboardView,
-    meta: setMeta(true, false), 
+    name: 'landing',
+    component: LandingView,
+    meta: { isGestOnly: true }
   },
   {
-    path: '/login',
-    component: Login,
-    meta: setMeta(false, true),
-  },
-  {
-    path: '/registration',
-    component: Registration,
-    meta: setMeta(false, true),
-  },
-  {
-    path: '/dashboard',
-    component: DashboardView,
-    meta: setMeta(true, false),
-  },
-  {
-      path: '/',
-      name: 'platform',
-      component: LandingView // 주소가 '/' 일 때 랜딩 페이지를 띄웁니다.
-    }
-]
+    path: '/platforms',
+    name: 'platform',
+    component: PlatformsView,
+    meta: { isAuthenticated: true }
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
+});
 
 router.beforeEach((to, from) => {
-
   const authStore = useAuthStore();
-
-  const isUserAuthenticated = authStore.isLoggedIn
+  const isUserAuthenticated = authStore.isLoggedIn;
 
   if (to.meta.isAuthenticated && !isUserAuthenticated) {
-    return '/login';
+    return '/';
   } 
   if (to.meta.isGestOnly && isUserAuthenticated) {
-    return '/';
+    return '/platforms';
   }
-})
+});
 
 export default router;
