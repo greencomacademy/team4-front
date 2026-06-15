c028393e0f7c092a934026b1e84b819697bc9da9
jeonminkyu <jeon950523@gmail.com>
2026-06-15 11:48:01 +0900

    Revert "메인페이지에서 라우터링크로 이동추가, 점포등록뷰추가"

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,17 +6,10 @@
 <div>
     <h1>배달 운영 정산 서비스</h1>
     <p>여러 배달 플랫폼의 주문과 수익을 한곳에서 관리합니다.</p>
-    <RouterLink to="/login">
+
     <button>로그인</button>
-    </RouterLink>
-
-    <RouterLink to="/signup">
     <button>점포 등록</button>
-    </RouterLink>
-
-    <RouterLink to="/dashboard">
-     <button>대시보드로 바로가기</button>
-    </RouterLink>
+    <RouterLink to="/dashboard">대시보드로 바로가기</RouterLink>
 </div>
 
 </template>
