cc26a8f4e35cfe5b516e320a12b9ff2ad9b8483f
minkyeong23 <choi777765@gmail.com>
2026-06-15 09:32:56 +0900

    로그인 회원가입 화면

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/auth/Login.vue'
+import Register from '../views/auth/Register.vue'
 
+const setMeta = (isAuthenticated, isGestOnly) => {
+  return {
+    isAuthenticated,
+    isGestOnly,
+  }
+}
+
+  const routes = [
+    // {
+    //   path: '/',
+    //   redirect: '/main', 
+    //   meta: setMeta(false, false),
+    // },
+    // {
+    //   path: '/main',
+    //   component: Main,
+    //   meta: setMeta(false, true),
+    // },
+
+
+    // auth
+    {
+      path: '/login',
+      component: Login,
+      meta: setMeta(false, true),
+    },
+
+    {
+      path: '/register',
+      component: Register,
+      meta: setMeta(false, true),
+    },
+
+
+  ]
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    // 지금은 빈 배열로 두어도 에러가 사라집니다. 나중에 3명이서 페이지를 붙일 때 여기에 추가합니다.
-  ]
+  routes,
 })
 
 export default router
---
+++ src/views/auth/Login.vue
@@ -0,0 +1,77 @@
+<script setup>
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+import loginValidator from '../../utils/validator/auth/loginValidator';
+import MyInput from '../../components/input/MyInput.vue';
+import Mybutton from '../../components/button/Mybutton.vue';
+
+const router = useRouter();
+
+const loginForm = reactive({
+  email: '',
+  password: '',
+});
+
+const hadleSubmit = async () => {
+  const resultValidationEmail = loginValidator.email(loginForm.email);
+  const resultValidationPassowrd = loginValidator.password(loginForm.password);
+
+}
+
+</script>
+
+<template>
+  <form @submit.prevent="hadleSubmit">
+    <h1>로그인</h1>
+    <div class="group">
+    <MyInput
+      :type="'email'"
+      :placeholder="'Email'"
+      :readonly="false"
+      :required="true"
+      v-model="loginForm.email"
+    ></MyInput>
+
+    <MyInput
+      :type="'password'"
+      :placeholder="'Password'"
+      :readonly="false"
+      :required="true"
+      v-model="loginForm.password"
+    ></MyInput>
+    </div>
+    
+    <Mybutton
+    :btnType="'submit'"
+    :color="'gray'"
+    :size="'small'"
+    :content="'로그인'"  
+    ></Mybutton>
+ 
+    <div class="sub-link">
+    <router-link to="/register" class="register">회원가입</router-link>
+    </div>
+  </form>
+
+</template>
+
+<style scoped>
+form {
+  padding: 20px 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+}
+
+.register {
+  font-size: 12px;
+  color: #666;
+  text-decoration: none;
+}
+
+.register:hover {
+  color: #333;
+}
+
+</style>
